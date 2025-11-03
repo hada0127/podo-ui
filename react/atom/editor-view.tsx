5740187ea33ac15f40655f46ba27acd41e2365f7
hada0127 <work@tarucy.net>
2025-11-04 07:54:14 +0900

    -

--- react/atom/editor-view.tsx
+++ react/atom/editor-view.tsx
@@ -1,9 +1,10 @@
 import 'suneditor/dist/css/suneditor.min.css';
+import styles from './editor-view.module.scss';
 
 const EditorView = ({ content }: { content: string }) => {
   return (
     <div
-      className="context sun-editor-editable"
+      className={`${styles.context} sun-editor-editable`}
       dangerouslySetInnerHTML={{ __html: content }}
     />
   );
