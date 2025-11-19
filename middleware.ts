64d968900bc392bbaaa3ff92a6a3aa947ebd561b
eunkyulee0127 <eklee@ourteam.kr>
2025-11-19 09:16:45 +0900

    fix: deploy

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+export const runtime = 'edge';
+
 export function middleware(request: NextRequest) {
   // Allow all requests to pass through
   return NextResponse.next();
